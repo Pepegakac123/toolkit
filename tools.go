d96e369abf2bf4202947fada5536c579c963bdd7
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-11-20 20:05:04 +0100

    Add WriteJSON func and add test for it

--- tools.go
+++ tools.go
@@ -183,6 +183,7 @@ type JSONResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
+// ReadJSON tries to read the body of teh req and converts it from json into go data variable
 func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1024 * 1024 // 1MB
 	if t.MaxJSONSize != 0 {
@@ -235,4 +236,24 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error
 
 	return nil
 
+}
+
+// WriteJSON takes a response status code and arbitrary data and writes json to client
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
 }
